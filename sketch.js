d522b3b8ea786e8035238b2eaa9b55d2b66ed2f2
Rizq <rizqyazed@gmail.com>
2021-10-02 07:29:07 +0000

    added colour change button

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let file;
 let img;
 let tiles, tileSize;
+var button;
+var colour = false;
 
 function handleFile(file) {
   print(file);
@@ -12,10 +14,20 @@ function handleFile(file) {
   }
 }
 
+function toggleColour() {
+  if (colour) {
+    colour = false;
+  } else if (!colour) {
+    colour = true;
+  }
+}
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(550, 550);
   input = createFileInput(handleFile);
   slider = createSlider(1, 45, 10, 0.5);
+  button = createButton("Colour");
+  button.mousePressed(toggleColour);
   colorMode(RGB, 255);
 
 }
@@ -27,7 +39,7 @@ function draw() {
 
   if (img) {
     // image(img, 0, 0, width, height);
-    img.resize(500,500)
+    img.resize(width,height)
     tiles = slider.value();
     tileSize = width/tiles;
     translate(tileSize/2, tileSize/2);
@@ -36,10 +48,14 @@ function draw() {
       for(y = 0; y < tiles; y++) {
   
         c = img.get(x*tileSize, y*tileSize);
-        size = map(brightness(c), 0, 255, 20, 0);
-        
-        fill(c);
-        ellipse(x*tileSize, y*tileSize, size, size);
+        size = map(brightness(c), 0, 100, 25, 0);
+        if (colour) {
+          fill(c);
+          ellipse(x*tileSize, y*tileSize, size, size);
+        } else {
+          fill(0);
+          ellipse(x*tileSize, y*tileSize, size, size);
+        }
       }
     }
   
